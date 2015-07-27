d42f075efbb905f7a063bd493a9ec8f13075b1b8
Mike Aanenson <mikeaanenson@exosite.com>
2015-07-27 10:53:28 -0500

    tidy up code

--- portals_api/upload_files_to_portals_file_system.py
+++ portals_api/upload_files_to_portals_file_system.py
@@ -3,26 +3,22 @@
 # Note: Uses Python 'Requests' module for calling API
 # APIs:
 # - http://docs.exosite.com/portals/#update-file-content
-
-
 import requests
 import getpass
 
-directory = "images" #default directory name
-domain = "" #example: example.exosite.com
-user_email = "" #example: myname@example.com - assume administrator access to Portals Domain Solution
+directory = "images" #default FS directory name
+domain = ""      #example: example.exosite.com
+user_email = ""    #example: myname@example.com - assume administrator access to Portals Domain Solution
 if domain == "":
 	domain = raw_input('Enter Portals Domain (e.g. "example.exosite.com": ')
 if user_email == "":
 	user_email = raw_input('Enter Your Email Address: ')
 user_password = getpass.getpass()  #ask for password each time at prompt
 
-
 # Files to upload
 files = {"MyLogo.png":open("./MyLogo.png", "rb"),
          "MyOtherLogo.jpg":open("./MyOtherLogo.jpg", "rb")
         }
-
 
 url = "https://"+domain+"/api/portals/v1/fs/"+directory
 
